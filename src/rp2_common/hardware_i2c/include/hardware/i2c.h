4598da45150d7f5eb76abe7c3f4b52d4f2fb0b03
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-01-31 17:54:43 +0900

    hardware_i2c apiを翻訳

--- src/rp2_common/hardware_i2c/include/hardware/i2c.h
+++ src/rp2_common/hardware_i2c/include/hardware/i2c.h
@@ -24,25 +24,31 @@ extern "C" {
 /** \file hardware/i2c.h
  *  \defgroup hardware_i2c hardware_i2c
  *
- * I2C Controller API
+ * \brief I2C制御API
  *
- * The I2C bus is a two-wire serial interface, consisting of a serial data line SDA and a serial clock SCL. These wires carry
- * information between the devices connected to the bus. Each device is recognized by a unique 7-bit address and can operate as
- * either a “transmitter” or “receiver”, depending on the function of the device. Devices can also be considered as masters or
- * slaves when performing data transfers. A master is a device that initiates a data transfer on the bus and generates the
- * clock signals to permit that transfer. The first byte in the data transfer always contains the 7-bit address and
- * a read/write bit in the LSB position. This API takes care of toggling the read/write bit. After this, any device addressed
- * is considered a slave. 
+ * I2Cバスは、シリアルデータラインSDAとシリアルクロックSCLからなる
+ * 2線式のシリアルインターフェースです。これらの線はバスに接続された
+ * デバイス間で情報を伝達します。各デバイスは一意の7ビットアドレスで
+ * 認識され、デバイスの機能に応じて「トランスミッタ」または「レシーバ」と
+ * して動作します。デバイスはまた、データ転送を実行する際にマスターまたは
+ * スレーブと見なすこともできます。マスターは、バス上でデータ転送を開始し、
+ * その転送を許可するクロック信号を生成するデバイスです。データ転送の最初の
+ * バイトは常に7ビットのアドレスとLSB位置にリード/ライトビットを含みます。
+ * このAPIはリード/ライトビットのトグル処理を行います。この後はアドレス指定
+ * されたすべてのデバイスはスレーブとみなされます。
  *
- * This API allows the controller to be set up as a master or a slave using the \ref i2c_set_slave_mode function.
+ * このAPIではコントローラは \ref i2c_set_slave_mode 関数を使ってマスター
+ * またはスレーブとして設定することができます。
  *
- * The external pins of each controller are connected to GPIO pins as defined in the GPIO muxing table in the datasheet. The muxing options
- * give some IO flexibility, but each controller external pin should be connected to only one GPIO.
+ * 各コントローラの外部ピンはデータシートのGPIO muxing tableで定義されている
+ * GPIOピンに接続されます。muxingオプションによりIOには柔軟性が与えられて
+ * いますが、各コントローラの外部ピンは1つのGPIOだけに接続する必要があります。
  *
- * Note that the controller does NOT support High speed mode or Ultra-fast speed mode, the fastest operation being fast mode plus
- * at up to 1000Kb/s.
+ * コントローラは高速モードと超高速モードをサポートしていないことに注意して
+ * ください。最速動作は最大1000Kb/秒のfastモードプラスです。
  *
- * See the datasheet for more information on the I2C controller and its usage.
+ * I2Cコントローラとその使用方法の詳細についてはデータシートを参照して
+ * ください。
  *
  * \subsection i2c_example Example
  * \addtogroup hardware_i2c
@@ -81,51 +87,51 @@ extern i2c_inst_t i2c1_inst;
 // ----------------------------------------------------------------------------
 // Setup
 
-/*! \brief   Initialise the I2C HW block
+/*! \brief   I2Cハードウェアブロックを初期化する
  *  \ingroup hardware_i2c
  *
- * Put the I2C hardware into a known state, and enable it. Must be called
- * before other functions. By default, the I2C is configured to operate as a
- * master.
+ * I2Cハードウェアを既知の状態に置き、有効にします。他の関数の前に
+ * 呼び出す必要があります。デフォルトでは、I2Cはマスターとして動作
+ * するように構成されます。
  *
- * The I2C bus frequency is set as close as possible to requested, and
- * the actual rate set is returned
+ * I2Cバス周波数には要求された値にできるだけ近い値が設定され、実際に
+ * 設定されたレートが返されます。
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param baudrate Baudrate in Hz (e.g. 100kHz is 100000)
- * \return Actual set baudrate
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param baudrate Hz単位のボーレート（たとえば、100kHz は 100000）
+ * \return 実際に設定されたボーレート
  */
 uint i2c_init(i2c_inst_t *i2c, uint baudrate);
 
-/*! \brief   Disable the I2C HW block
+/*! \brief   I2Cハードウェアブロックを無効にする
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
  *
- * Disable the I2C again if it is no longer used. Must be reinitialised before
- * being used again.
+ * I2Cが使用されなくなった場合は再度無効にします。再度使用する場合は
+ * 再初期化する必要があります。
  */
 void i2c_deinit(i2c_inst_t *i2c);
 
-/*! \brief  Set I2C baudrate
+/*! \brief  I2Cのボーレートを設定する
  *  \ingroup hardware_i2c
  *
- * Set I2C bus frequency as close as possible to requested, and return actual
- * rate set.
- * Baudrate may not be as exactly requested due to clocking limitations.
+ * I2Cバス周波数に要求された値にできるだけ近い値を設定し、実際に
+ * 設定されたレートを返します。クロックの制限により、ボーレートは
+ * 要求通りの値にならない場合もあります。
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param baudrate Baudrate in Hz (e.g. 100kHz is 100000)
- * \return Actual set baudrate
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param baudrate Hz単位のボーレート（たとえば、100kHz は 100000）
+ * \return 実際に設定されたボーレート
  */
 uint i2c_set_baudrate(i2c_inst_t *i2c, uint baudrate);
 
-/*! \brief  Set I2C port to slave mode
+/*! \brief  I2Cポートをスレーブモードにセットする
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param slave true to use slave mode, false to use master mode
- * \param addr If \p slave is true, set the slave address to this value
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param slave スレーブモードとして使う場合は true, マスターモードとして使う場合は false
+ * \param addr If \p slave が true の場合はスレーブアドレスをセットする
  */
 void i2c_set_slave_mode(i2c_inst_t *i2c, bool slave, uint8_t addr);
 
@@ -137,11 +143,11 @@ struct i2c_inst {
     bool restart_on_next;
 };
 
-/*! \brief Convert I2C instance to hardware instance number
+/*! \brief I2Cインスタンスをハードウェアインスタンス番号に変換する
  *  \ingroup hardware_i2c
  *
- * \param i2c I2C instance
- * \return Number of I2C, 0 or 1.
+ * \param i2c I2Cインスタンス
+ * \return I2Cの番号, 0 か 1.
  */
 static inline uint i2c_hw_index(i2c_inst_t *i2c) {
     invalid_params_if(I2C, i2c != i2c0 && i2c != i2c1);
@@ -159,51 +165,59 @@ static inline i2c_inst_t *i2c_get_instance(uint instance) {
     return instance ? i2c1 : i2c0;
 }
 
-/*! \brief Attempt to write specified number of bytes to address, blocking until the specified absolute time is reached.
+/*! \brief 指定された絶対時間に達するまでブロックして、指定したアドレスへの指定したバイト数の書き込みを試みる.
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param addr 7-bit address of device to write to
- * \param src Pointer to data to send
- * \param len Length of data in bytes to send
- * \param nostop  If true, master retains control of the bus at the end of the transfer (no Stop is issued),
- *           and the next transfer will begin with a Restart rather than a Start.
- * \param until The absolute time that the block will wait until the entire transaction is complete. Note, an individual timeout of
- *           this value divided by the length of data is applied for each byte transfer, so if the first or subsequent
- *           bytes fails to transfer within that sub timeout, the function will return with an error.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param addr 書き込み先のデバイスの7ビットアドレス
+ * \param src 送信するデータへのポインタ
+ * \param len 送信するデータのバイト帳
+ * \param nostop  true の場合、マスターは転送終了時にバスの制御を保持し
+ * （Stopを発行しない）、StartではなくRestartで次の転送を開始する。
+ * \param until トランザクション全体が完了するまでブロックが待機する絶対時間。
+ * この値をデータ長で割った値が各バイトの転送に適用されるため、最初のバイト
+ * またはそれ以降のバイトがそのサブタイムアウト内に転送できなかった場合、
+ * この関数はエラーで返されることに注意されたい。
  *
- * \return Number of bytes written, or PICO_ERROR_GENERIC if address not acknowledged, no device present, or PICO_ERROR_TIMEOUT if a timeout occurred.
+ * \return 書き込まれたバイト数. または、アドレスが認識されなかった、または、
+ * デバイスが存在しない場合は PICO_ERROR_GENERIC、タイムアウトが発生した場合は
+ * PICO_ERROR_TIMEOUT
  */
 int i2c_write_blocking_until(i2c_inst_t *i2c, uint8_t addr, const uint8_t *src, size_t len, bool nostop, absolute_time_t until);
 
-/*! \brief  Attempt to read specified number of bytes from address, blocking until the specified absolute time is reached.
+/*! \brief  指定された絶対時間に達するまでブロックして、指定したアドレスから指定したバイト数の読み込みを試みる.
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param addr 7-bit address of device to read from
- * \param dst Pointer to buffer to receive data
- * \param len Length of data in bytes to receive
- * \param nostop  If true, master retains control of the bus at the end of the transfer (no Stop is issued),
- *           and the next transfer will begin with a Restart rather than a Start.
- * \param until The absolute time that the block will wait until the entire transaction is complete.
- * \return Number of bytes read, or PICO_ERROR_GENERIC if address not acknowledged, no device present, or PICO_ERROR_TIMEOUT if a timeout occurred.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param addr 読み込むデバイスの7ビットアドレス
+ * \param dst データを受信するバッファへのポインタ
+ * \param len 受信するデータのバイト長
+ * \param nostop  true の場合、マスターは転送終了時にバスの制御を保持し
+ * （Stopを発行しない）、StartではなくRestartで次の転送を開始する。
+ * \param until Tトランザクション全体が完了するまでブロックが待機する絶対時間。
+ * \return 読み込んだバイト数. または、アドレスが認識されなかった、または、
+ * デバイスが存在しない場合は PICO_ERROR_GENERIC、タイムアウトが発生した場合は
+ * PICO_ERROR_TIMEOUT.
  */
 int i2c_read_blocking_until(i2c_inst_t *i2c, uint8_t addr, uint8_t *dst, size_t len, bool nostop, absolute_time_t until);
 
-/*! \brief Attempt to write specified number of bytes to address, with timeout
+/*! \brief タイムアウトを指定して指定したアドレスへの指定したバイト数の書き込みを試みる.
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param addr 7-bit address of device to write to
- * \param src Pointer to data to send
- * \param len Length of data in bytes to send
- * \param nostop  If true, master retains control of the bus at the end of the transfer (no Stop is issued),
- *           and the next transfer will begin with a Restart rather than a Start.
- * \param timeout_us The time that the function will wait for the entire transaction to complete. Note, an individual timeout of
- *           this value divided by the length of data is applied for each byte transfer, so if the first or subsequent
- *           bytes fails to transfer within that sub timeout, the function will return with an error.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param addr 書き込み先のデバイスの7ビットアドレス
+ * \param src 送信するデータへのポインタ
+ * \param len 送信するデータのバイト帳
+ * \param nostop  true の場合、マスターは転送終了時にバスの制御を保持し
+ * （Stopを発行しない）、StartではなくRestartで次の転送を開始する。
+ * \param timeout_us トランザクション全体が完了するまでブロックが待機する絶対時間。
+ * この値をデータ長で割った値が各バイトの転送に適用されるため、最初のバイト
+ * またはそれ以降のバイトがそのサブタイムアウト内に転送できなかった場合、
+ * この関数はエラーで返されることに注意されたい。
  *
- * \return Number of bytes written, or PICO_ERROR_GENERIC if address not acknowledged, no device present, or PICO_ERROR_TIMEOUT if a timeout occurred.
+ * \return 書き込まれたバイト数. または、アドレスが認識されなかった、または、
+ * デバイスが存在しない場合は PICO_ERROR_GENERIC、タイムアウトが発生した場合は
+ * PICO_ERROR_TIMEOUT
  */
 static inline int i2c_write_timeout_us(i2c_inst_t *i2c, uint8_t addr, const uint8_t *src, size_t len, bool nostop, uint timeout_us) {
     absolute_time_t t = make_timeout_time_us(timeout_us);
@@ -212,17 +226,19 @@ static inline int i2c_write_timeout_us(i2c_inst_t *i2c, uint8_t addr, const uint
 
 int i2c_write_timeout_per_char_us(i2c_inst_t *i2c, uint8_t addr, const uint8_t *src, size_t len, bool nostop, uint timeout_per_char_us);
 
-/*! \brief  Attempt to read specified number of bytes from address, with timeout
+/*! \brief  タイムアウトを指定して指定したアドレスから指定したバイト数の読み込みを試みる.
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param addr 7-bit address of device to read from
- * \param dst Pointer to buffer to receive data
- * \param len Length of data in bytes to receive
- * \param nostop  If true, master retains control of the bus at the end of the transfer (no Stop is issued),
- *           and the next transfer will begin with a Restart rather than a Start.
- * \param timeout_us The time that the function will wait for the entire transaction to complete
- * \return Number of bytes read, or PICO_ERROR_GENERIC if address not acknowledged, no device present, or PICO_ERROR_TIMEOUT if a timeout occurred.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param addr 読み込むデバイスの7ビットアドレス
+ * \param dst データを受信するバッファへのポインタ
+ * \param len 受信するデータのバイト長
+ * \param nostop  true の場合、マスターは転送終了時にバスの制御を保持し
+ * （Stopを発行しない）、StartではなくRestartで次の転送を開始する。
+ * \param timeout_us トランザクション全体が完了するまで関数が待機する時間
+ * \return 読み込んだバイト数. または、アドレスが認識されなかった、または、
+ * デバイスが存在しない場合は PICO_ERROR_GENERIC、タイムアウトが発生した場合は
+ * PICO_ERROR_TIMEOUT.
  */
 static inline int i2c_read_timeout_us(i2c_inst_t *i2c, uint8_t addr, uint8_t *dst, size_t len, bool nostop, uint timeout_us) {
     absolute_time_t t = make_timeout_time_us(timeout_us);
@@ -231,65 +247,66 @@ static inline int i2c_read_timeout_us(i2c_inst_t *i2c, uint8_t addr, uint8_t *ds
 
 int i2c_read_timeout_per_char_us(i2c_inst_t *i2c, uint8_t addr, uint8_t *dst, size_t len, bool nostop, uint timeout_per_char_us);
 
-/*! \brief Attempt to write specified number of bytes to address, blocking
+/*! \brief ブロックして指定したアドレスへの指定したバイト数の書き込みを試みる.
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param addr 7-bit address of device to write to
- * \param src Pointer to data to send
- * \param len Length of data in bytes to send
- * \param nostop  If true, master retains control of the bus at the end of the transfer (no Stop is issued),
- *           and the next transfer will begin with a Restart rather than a Start.
- * \return Number of bytes written, or PICO_ERROR_GENERIC if address not acknowledged, no device present.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param addr 書き込み先のデバイスの7ビットアドレス
+ * \param src 送信するデータへのポインタ
+ * \param len 送信するデータのバイト帳
+ * \param nostop  true の場合、マスターは転送終了時にバスの制御を保持し
+ * （Stopを発行しない）、StartではなくRestartで次の転送を開始する。
+ * \return 書き込まれたバイト数. または、アドレスが認識されなかった、または、
+ * デバイスが存在しない場合は PICO_ERROR_GENERIC.
  */
 int i2c_write_blocking(i2c_inst_t *i2c, uint8_t addr, const uint8_t *src, size_t len, bool nostop);
 
-/*! \brief  Attempt to read specified number of bytes from address, blocking
+/*! \brief  ブロックして指定したアドレスから指定したバイト数の読み込みを試みる.
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param addr 7-bit address of device to read from
- * \param dst Pointer to buffer to receive data
- * \param len Length of data in bytes to receive
- * \param nostop  If true, master retains control of the bus at the end of the transfer (no Stop is issued),
- *           and the next transfer will begin with a Restart rather than a Start.
- * \return Number of bytes read, or PICO_ERROR_GENERIC if address not acknowledged or no device present.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param addr 読み込むデバイスの7ビットアドレス
+ * \param dst データを受信するバッファへのポインタ
+ * \param len 受信するデータのバイト長
+ * \param nostop  true の場合、マスターは転送終了時にバスの制御を保持し
+ * （Stopを発行しない）、StartではなくRestartで次の転送を開始する。
+ * \return 読み込んだバイト数. または、アドレスが認識されなかった、または、
+ * デバイスが存在しない場合は PICO_ERROR_GENERIC.
  */
 int i2c_read_blocking(i2c_inst_t *i2c, uint8_t addr, uint8_t *dst, size_t len, bool nostop);
 
 
-/*! \brief Determine non-blocking write space available
+/*! \brief ノンブロッキングの書き込みスペースがあるかチェックする
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \return 0 if no space is available in the I2C to write more data. If return is nonzero, at
- * least that many bytes can be written without blocking.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \return これ以上データを書き込むスペースがI2Cにない場合は 0. 0以外の
+ * 場合は、少なくともこのバイト数だけはブロックなしで書き込むことができる.
  */
 static inline size_t i2c_get_write_available(i2c_inst_t *i2c) {
     const size_t IC_TX_BUFFER_DEPTH = 16;
     return IC_TX_BUFFER_DEPTH - i2c_get_hw(i2c)->txflr;
 }
 
-/*! \brief Determine number of bytes received
+/*! \brief 受信できるバイト数をチェックする
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \return 0 if no data available, if return is nonzero at
- * least that many bytes can be read without blocking.
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \return データがない場合は 0, 0 以外の場合は少なくともこのバイト数
+ * だけはブロックなしで読み込むことができる.
  */
 static inline size_t i2c_get_read_available(i2c_inst_t *i2c) {
     return i2c_get_hw(i2c)->rxflr;
 }
 
-/*! \brief Write direct to TX FIFO
+/*! \brief TX FIFOに直接書き込む
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param src Data to send
- * \param len Number of bytes to send
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param src 送信するデータ
+ * \param len 送信されたバイト数
  *
- * Writes directly to the I2C TX FIFO which is mainly useful for
- * slave-mode operation.
+ * I2C FIFOに直接書き込みます。主にスレーブモードの操作に便利です。
  */
 static inline void i2c_write_raw_blocking(i2c_inst_t *i2c, const uint8_t *src, size_t len) {
     for (size_t i = 0; i < len; ++i) {
@@ -300,15 +317,14 @@ static inline void i2c_write_raw_blocking(i2c_inst_t *i2c, const uint8_t *src, s
     }
 }
 
-/*! \brief Read direct from RX FIFO
+/*! \brief RX FIFOから直接読み込む
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param dst Buffer to accept data
- * \param len Number of bytes to read
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param dst データを受け取るバッファ
+ * \param len 読み込むバイト数
  *
- * Reads directly from the I2C RX FIFO which is mainly useful for
- * slave-mode operation.
+ * I2C RX FIFOから直接読み込みます。主にスレーブモードの操作に便利です。
  */
 static inline void i2c_read_raw_blocking(i2c_inst_t *i2c, uint8_t *dst, size_t len) {
     for (size_t i = 0; i < len; ++i) {
@@ -319,13 +335,13 @@ static inline void i2c_read_raw_blocking(i2c_inst_t *i2c, uint8_t *dst, size_t l
 }
 
 /**
- * \brief Pop a byte from I2C Rx FIFO.
+ * \brief I2C Rx FIFOから1バイト取り出す
  * \ingroup hardware_i2c
  *
- * This function is non-blocking and assumes the Rx FIFO isn't empty.
+ * この関数はノンブロッキングです。RX FIFOが空でないことを仮定しています。
  *
- * \param i2c I2C instance.
- * \return uint8_t Byte value.
+ * \param i2c I2Cインスタンス.
+ * \return uint8_t バイト値.
  */
 static inline uint8_t i2c_read_byte_raw(i2c_inst_t *i2c) {
     i2c_hw_t *hw = i2c_get_hw(i2c);
@@ -334,13 +350,13 @@ static inline uint8_t i2c_read_byte_raw(i2c_inst_t *i2c) {
 }
 
 /**
- * \brief Push a byte into I2C Tx FIFO.
+ * \brief I2C Tx FIFOに1バイト置く
  * \ingroup hardware_i2c
  *
- * This function is non-blocking and assumes the Tx FIFO isn't full.
+ * この関数はノンブロッキングです。TX FIFOがフルでないことを仮定しています。
  *
- * \param i2c I2C instance.
- * \param value Byte value.
+ * \param i2c I2Cインスタンス.
+ * \param value バイト値.
  */
 static inline void i2c_write_byte_raw(i2c_inst_t *i2c, uint8_t value) {
     i2c_hw_t *hw = i2c_get_hw(i2c);
@@ -349,11 +365,11 @@ static inline void i2c_write_byte_raw(i2c_inst_t *i2c, uint8_t value) {
 }
 
 
-/*! \brief Return the DREQ to use for pacing transfers to/from a particular I2C instance
+/*! \brief 指定のI2Cインスタンスとの間の転送のペース設定に使用するDREQを返す
  *  \ingroup hardware_i2c
  *
- * \param i2c Either \ref i2c0 or \ref i2c1
- * \param is_tx true for sending data to the I2C instance, false for receiving data from the I2C instance
+ * \param i2c \ref i2c0 か \ref i2c1 のいずれか
+ * \param is_tx I2Cインスタンスにデータを送信する場合は true, 受信する場合は false
  */
 static inline uint i2c_get_dreq(i2c_inst_t *i2c, bool is_tx) {
     static_assert(DREQ_I2C0_RX == DREQ_I2C0_TX + 1, "");
