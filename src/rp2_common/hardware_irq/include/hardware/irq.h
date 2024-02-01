b037864e4e3952bbba11357ae9afd378d4cf44d5
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-02-01 11:26:39 +0900

    hardware_irq apiを翻訳

--- src/rp2_common/hardware_irq/include/hardware/irq.h
+++ src/rp2_common/hardware_irq/include/hardware/irq.h
@@ -33,38 +33,51 @@
 /** \file irq.h
  *  \defgroup hardware_irq hardware_irq
  *
- * Hardware interrupt handling
+ * \brief ハードウェア割り込み処理
  *
- * The RP2040 uses the standard ARM nested vectored interrupt controller (NVIC).
+ * RP2040は標準的なARM NVIC (Nested Vectored Interrupt Controller) を
+ * 使用しています。
  *
- * Interrupts are identified by a number from 0 to 31.
+ * 割り込みは0から31までの番号で識別されます。
  *
- * On the RP2040, only the lower 26 IRQ signals are connected on the NVIC; IRQs 26 to 31 are tied to zero (never firing).
+ * RP2040では下位26までのIRQ信号だけがNVICに接続されており、IRQ 26から31は
+ * ゼロに束縛されています（発火することはありません）。
  *
- * There is one NVIC per core, and each core's NVIC has the same hardware interrupt lines routed to it, with the exception of the IO interrupts
- * where there is one IO interrupt per bank, per core. These are completely independent, so, for example, processor 0 can be
- * interrupted by GPIO 0 in bank 0, and processor 1 by GPIO 1 in the same bank.
+ * NVICはコアごとに1つあり、各コアのNVICには同じハードウェア割り込み線が
+ * ルーティングされていますが、IO割り込みは例外であり、コアごと、バンクごとに
+ * 1つIO割り込みがあります。これらは完全に独立しているため、たとえば、
+ * プロセッサ 0はバンク 0のGPIO 0からの割り込みを受け、プロセッサ 1は同じ
+ * バンクのGPIO 1からの割り込みを受け付けることができます。
  *
- * \note That all IRQ APIs affect the executing core only (i.e. the core calling the function).
+ * \note すべてのIRQ APIは実行中のコア（すなわち、関数を呼び出しているコア）
+ * だけに影響します。
  *
- * \note You should not enable the same (shared) IRQ number on both cores, as this will lead to race conditions
- * or starvation of one of the cores. Additionally, don't forget that disabling interrupts on one core does not disable interrupts
- * on the other core.
+ * 両方のコアで同じ（共有の）IRQ番号を有効にしてはいけません。なぜなら、
+ * 競合条件やコアの1つでのスターベーションにつながるからです。さらに、ある
+ * コアで割り込みを無効にしても他のコアでの割り込みは無効にならないことを
+ * 忘れないでください。
  *
- * There are three different ways to set handlers for an IRQ:
- *  - Calling irq_add_shared_handler() at runtime to add a handler for a multiplexed interrupt (e.g. GPIO bank) on the current core. Each handler, should check and clear the relevant hardware interrupt source
- *  - Calling irq_set_exclusive_handler() at runtime to install a single handler for the interrupt on the current core
- *  - Defining the interrupt handler explicitly in your application (e.g. by defining void `isr_dma_0` will make that function the handler for the DMA_IRQ_0 on core 0, and
- *    you will not be able to change it using the above APIs at runtime). Using this method can cause link conflicts at runtime, and offers no runtime performance benefit (i.e, it should not generally be used).
+ * IRQにハンドラを設定する方法は3つあります。
+ * - 実行時に irq_add_shared_handler() を呼び出して、現在のコアの多重化
+ *   割り込み（GPIOバンクなど）のハンドラを追加する。各ハンドラは関連する
+ *   ハードウェア割り込みソースをチェックし、クリアする必要があります。
+ * - 実行時に irq_set_exclusive_handler() を呼び出して、現在のコアに割り込み
+ *   ハンドラを1つインストールする。
+ * - アプリケーションで割り込みハンドラを明示的に定義する（たとえば、
+ *   void `isr_dma_0` を定義するとその関数はコア 0のDMA_IRQ_0のハンドラとなり、
+ *   実行時に上記のAPIを使用して変更することはできません）。この関数を
+ *   使用すると実行時にリンクの競合が発生する可能性があり、実行時の性能には
+ *   何のメリットもありません（つまり、一般には使用すべきではありません）。
  *
- * \note If an IRQ is enabled and fires with no handler installed, a breakpoint will be hit and the IRQ number will
- * be in register r0.
+ * \note IRQが有効化され、ハンドラがインストールされていない状態で発火すると、
+ * ブレークポイントがヒットし、IRQ番号がレジスタr0に設定されます。
  *
- * \section interrupt_nums Interrupt Numbers
+ * \section interrupt_nums 割り込み番号
  *
- * Interrupts are numbered as follows, a set of defines is available (intctrl.h) with these names to avoid using the numbers directly.
+ * 割り込み番号は以下の通りに振られています。これらの番号を直接使わないように
+ * するためにその名前を持つ一連の定義 (intctrl.h) が用意されています。
  *
- * IRQ | Interrupt Source
+ * IRQ | 割り込みソース
  * ----|-----------------
  *  0 | TIMER_IRQ_0
  *  1 | TIMER_IRQ_1
@@ -120,10 +133,11 @@
 extern "C" {
 #endif
 
-/*! \brief Interrupt handler function type
+/*! \brief 割り込みハンドラ関数型
  *  \ingroup hardware_irq
  *
- * All interrupts handlers should be of this type, and follow normal ARM EABI register saving conventions
+ * 割り込みハンドラはすべてこの型でなければなりません。また、標準的な
+ * ARM EABIレジスタ保存規則に従う必要があります。
  */
 typedef void (*irq_handler_t)(void);
 
@@ -131,234 +145,251 @@ static inline void check_irq_param(__unused uint num) {
     invalid_params_if(IRQ, num >= NUM_IRQS);
 }
 
-/*! \brief Set specified interrupt's priority
+/*! \brief 指定した割り込みの優先度をセットする
  *  \ingroup hardware_irq
  *
- * \param num Interrupt number \ref interrupt_nums
- * \param hardware_priority Priority to set.
- * Numerically-lower values indicate a higher priority. Hardware priorities
- * range from 0 (highest priority) to 255 (lowest priority) though only the
- * top 2 bits are significant on ARM Cortex-M0+. To make it easier to specify
- * higher or lower priorities than the default, all IRQ priorities are
- * initialized to PICO_DEFAULT_IRQ_PRIORITY by the SDK runtime at startup.
- * PICO_DEFAULT_IRQ_PRIORITY defaults to 0x80
+ * \param num 割り込み番号 \ref interrupt_nums
+ * \param hardware_priority セットする優先度.
+ * 数値が小さいほど優先度が高いことを示します。ハードウェアの優先度は0
+ * （最高優先度）から255（最低優先度）の範囲ですが、ARM Cortex-M0+では
+ * 上位2ビットだけが有効です。デフォルトよりも高い優先度や低い優先度を簡単に
+ * 指定できるように起動時にSDKランタイムによってすべてのIRQの優先度が
+ * PICO_DEFAULT_IRQ_PRIORITYに初期化されます。PICO_DEFAULT_IRQ_PRIORITYの
+ * デフォルトは0x80です。
  */
 void irq_set_priority(uint num, uint8_t hardware_priority);
 
-/*! \brief Get specified interrupt's priority
+/*! \brief 指定の割り込みの優先度を取得する
  *  \ingroup hardware_irq
  *
- * Numerically-lower values indicate a higher priority. Hardware priorities
- * range from 0 (highest priority) to 255 (lowest priority) though only the
- * top 2 bits are significant on ARM Cortex-M0+. To make it easier to specify
- * higher or lower priorities than the default, all IRQ priorities are
- * initialized to PICO_DEFAULT_IRQ_PRIORITY by the SDK runtime at startup.
- * PICO_DEFAULT_IRQ_PRIORITY defaults to 0x80
+ * 数値が小さいほど優先度が高いことを示します。ハードウェアの優先度は0
+ * （最高優先度）から255（最低優先度）の範囲ですが、ARM Cortex-M0+では
+ * 上位2ビットだけが有効です。デフォルトよりも高い優先度や低い優先度を簡単に
+ * 指定できるように起動時にSDKランタイムによってすべてのIRQの優先度が
+ * PICO_DEFAULT_IRQ_PRIORITYに初期化されます。PICO_DEFAULT_IRQ_PRIORITYの
+ * デフォルトは0x80です。
  *
- * \param num Interrupt number \ref interrupt_nums
- * \return the IRQ priority
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \return IRQの優先度
  */
 uint irq_get_priority(uint num);
 
-/*! \brief Enable or disable a specific interrupt on the executing core
+/*! \brief 実行中のコアにおける指定の割り込みを有効/無効にする
  *  \ingroup hardware_irq
  *
- * \param num Interrupt number \ref interrupt_nums
- * \param enabled true to enable the interrupt, false to disable
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \param enabled 割り込みを有効にする場合は true, 無効にする場合は false
  */
 void irq_set_enabled(uint num, bool enabled);
 
-/*! \brief Determine if a specific interrupt is enabled on the executing core
+/*! \brief 指定の割り込みが実行中のコアにおいて有効であるかチェックする
  *  \ingroup hardware_irq
  *
- * \param num Interrupt number \ref interrupt_nums
- * \return true if the interrupt is enabled
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \return 割り込みが有効である場合は true
  */
 bool irq_is_enabled(uint num);
 
-/*! \brief Enable/disable multiple interrupts on the executing core
+/*! \brief 実行中のコアにおける複数の割り込みを有効/無効にする
  *  \ingroup hardware_irq
  *
- * \param mask 32-bit mask with one bits set for the interrupts to enable/disable \ref interrupt_nums
- * \param enabled true to enable the interrupts, false to disable them.
+ * \param mask 32ビットのマスク. 各ビットが対象の割り込みの有効/無効を設定する \ref interrupt_nums
+ * \param enabled 割り込みを有効にする場合は true, 無効にする場合は false
  */
 void irq_set_mask_enabled(uint32_t mask, bool enabled);
 
-/*! \brief  Set an exclusive interrupt handler for an interrupt on the executing core.
+/*! \brief 実行中のコアにおける割り込みに対して排他的な割り込みハンドラをセットする
  *  \ingroup hardware_irq
  *
- * Use this method to set a handler for single IRQ source interrupts, or when
- * your code, use case or performance requirements dictate that there should
- * no other handlers for the interrupt.
+ * この関数は単一のIRQソースの割り込みハンドラを設定する場合、または、あなたの
+ * コード、ユースケース、パ フォーマンス要件がその割り込みに対して他の
+ * ハンドラが存在しないことを指示する場合に使用してください。
  *
- * This method will assert if there is already any sort of interrupt handler installed
- * for the specified irq number.
+ * この関数は指定したIRQ番号に対して何らかの割り込みハンドラが既に
+ * インストールされている場合にアサートします。
  *
- * \param num Interrupt number \ref interrupt_nums
- * \param handler The handler to set. See \ref irq_handler_t
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \param handler セットするハンドラ. \ref irq_handler_t を参照
  * \see irq_add_shared_handler()
  */
 void irq_set_exclusive_handler(uint num, irq_handler_t handler);
 
-/*! \brief  Get the exclusive interrupt handler for an interrupt on the executing core.
+/*! \brief  実行中のコアにおける指定の割り込みに対する排他的な割り込みハンドラを取得する.
  *  \ingroup hardware_irq
  *
- * This method will return an exclusive IRQ handler set on this core
- * by irq_set_exclusive_handler if there is one.
+ * この関数は存在すれば irq_set_exclusive_handler によってこのコアに対して
+ * セットされた排他的IRQハンドラを返します。
  *
- * \param num Interrupt number \ref interrupt_nums
+ * \param num 割り込み場号 \ref interrupt_nums
  * \see irq_set_exclusive_handler()
- * \return handler The handler if an exclusive handler is set for the IRQ,
- *                 NULL if no handler is set or shared/shareable handlers are installed
+ * \return 指定のIRQに排他的ハンドラがセットされている場合はそのハンドラ,
+ * ハンドラがセットされていない、または、共有/共有可能ハンドラが
+ * インストールされている場合は NULL
  */
 irq_handler_t irq_get_exclusive_handler(uint num);
 
-/*! \brief Add a shared interrupt handler for an interrupt on the executing core
+/*! \brief 実行中のコアにおける割り込みに対して共有の割り込みハンドラをセットする
  *  \ingroup hardware_irq
  *
- * Use this method to add a handler on an irq number shared between multiple distinct hardware sources (e.g. GPIO, DMA or PIO IRQs).
- * Handlers added by this method will all be called in sequence from highest order_priority to lowest. The
- * irq_set_exclusive_handler() method should be used instead if you know there will or should only ever be one handler for the interrupt.
+ * 複数の異なるハードウェアソース（GPIO、DMA、PIO IRQなど）で共有されるIRQ
+ * 番号にハンドラを追加するにはこの関数を使用してください。この関数で追加
+ * されたハンドラはorder_priority の高いものから低いものへと順番にすべて
+ * 呼び出されます。割り込みのハンドラが1つしかない、または1つしかあっては
+ * いけないことが分かっている場合はこの関数の代わりに
+ * irq_set_exclusive_handler() を使用する必要があります。
  *
- * This method will assert if there is an exclusive interrupt handler set for this irq number on this core, or if
- * the (total across all IRQs on both cores) maximum (configurable via PICO_MAX_SHARED_IRQ_HANDLERS) number of shared handlers
- * would be exceeded.
+ * この関数はこのコアにおける指定のIRQ番号に排他的な割り込みハンドラが設定
+ * されている場合、または、（両コアにおけるすべてのIRQを合計した）共有
+ * ハンドラの数が最大値（PICO_MAX_SHARED_IRQ_HANDLERS で設定可能）を超える
+ * 場合にアサートします。
  *
- * \param num Interrupt number \ref interrupt_nums
- * \param handler The handler to set. See \ref irq_handler_t
- * \param order_priority The order priority controls the order that handlers for the same IRQ number on the core are called.
- * The shared irq handlers for an interrupt are all called when an IRQ fires, however the order of the calls is based
- * on the order_priority (higher priorities are called first, identical priorities are called in undefined order). A good
- * rule of thumb is to use PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY if you don't much care, as it is in the middle of
- * the priority range by default.
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \param handler セットするハンドラ. \ref irq_handler_t を参照
+ * \param order_priority order_priorityは、コアのいける同じIRQ番号の
+ * ハンドラが呼び出される順番を制御します。割り込みの共有IRQハンドラは
+ * IRQが発火するとすべて呼び出されますが、呼び出し順序は order_priority に
+ * 基づいています（高い優先度が最初に呼び出されます。同じ優先度の
+ * 呼び出し順位は未定義です）。あまり気にしないのであれば優先度の
+ * 真ん中であるPICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITYを使用
+ * するのが良い経験則です。
  *
- * \note The order_priority uses \em higher values for higher priorities which is the \em opposite of the CPU interrupt priorities passed
- * to irq_set_priority() which use lower values for higher priorities.
+ * \note order_priority は高い優先度に \em 大きな 値を使用しますが、
+ * これは 高い優先度も小さな値を使用する irq_set_priority()に渡される
+ * CPU割り込みの優先度とは \em 真逆 です。
  *
  * \see irq_set_exclusive_handler()
  */
 void irq_add_shared_handler(uint num, irq_handler_t handler, uint8_t order_priority);
 
-/*! \brief Remove a specific interrupt handler for the given irq number on the executing core
+/*! \brief 実行中のコアにおける指定のIRQ番号の指定の割り込みハンドラを削除する
  *  \ingroup hardware_irq
  *
- * This method may be used to remove an irq set via either irq_set_exclusive_handler() or
- * irq_add_shared_handler(), and will assert if the handler is not currently installed for the given
- * IRQ number
+ * この関数はirq_set_exclusive_handler(), または irq_add_shared_handler() で
+ * セットされたIRQハンドラの削除に使うことができます。指定のIRQ番号に現在
+ * ハンドラがインストールされていない場合はアサートします。
  *
- * \note This method may *only* be called from user (non IRQ code) or from within the handler
- * itself (i.e. an IRQ handler may remove itself as part of handling the IRQ). Attempts to call
- * from another IRQ will cause an assertion.
+ * \note この関数はユーザ（IRQコードでない）、または、ハンドラ自身の内部
+ * （すなわち、IRQハンドラがIRQの処理の一部として自分を削除する）から
+ * *しか* 呼び出すことができません。他のIRQからの呼び出しはアサートを
+ * 引き起こします。
  *
- * \param num Interrupt number \ref interrupt_nums
- * \param handler The handler to removed.
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \param handler 削除するハンドラ.
  * \see irq_set_exclusive_handler()
  * \see irq_add_shared_handler()
  */
 void irq_remove_handler(uint num, irq_handler_t handler);
 
-/*! \brief Determine if the current handler for the given number is shared
+/*! \brief 指定したIRQ番号に現在共有ハンドラがセットされているかチェックする
  *  \ingroup hardware_irq
  *
- * \param num Interrupt number \ref interrupt_nums
- * \return true if the specified IRQ has a shared handler
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \return 指定したIRQが共有ハンドラを持っている場合は true
  */
 bool irq_has_shared_handler(uint num);
 
-/*! \brief Get the current IRQ handler for the specified IRQ from the currently installed hardware vector table (VTOR)
- * of the execution core
+/*! \brief 指定したIRQの現在のIRQハンドラを現在実行中のコアにインストールされているハードウェアベクタテーブル（VTOR)から取得する
  *  \ingroup hardware_irq
  *
- * \param num Interrupt number \ref interrupt_nums
- * \return the address stored in the VTABLE for the given irq number
+ * \param num 割り込み場号 \ref interrupt_nums
+ * \return 指定したIRQ番号用にVTABLEに格納されているアドレス
  */
 irq_handler_t irq_get_vtable_handler(uint num);
 
-/*! \brief Clear a specific interrupt on the executing core
+/*! \brief 実行中のコアにおける指定の割り込みをクリアする
  *  \ingroup hardware_irq
  *
- * This method is only useful for "software" IRQs that are not connected to hardware (i.e. IRQs 26-31)
- * as the the NVIC always reflects the current state of the IRQ state of the hardware for hardware IRQs, and clearing
- * of the IRQ state of the hardware is performed via the hardware's registers instead.
+ * この関数はハードウェアに接続されていない"ソフトウェア"IRQ（すなわち、
+ * IRQ 26～31）に対してのみ有効です。なぜなら、NVICは常にハードウェアIRQに
+ * 対してハードウェアのIRQ状態の現在の状態を反映しており、ハードウェアの
+ * IRQ状態のクリアはハードウェアのレジスタを介して実行されるからです。
  *
- * \param int_num Interrupt number \ref interrupt_nums
+ * \param int_num 割り込み場号 \ref interrupt_nums
  */
 static inline void irq_clear(uint int_num) {
     *((volatile uint32_t *) (PPB_BASE + M0PLUS_NVIC_ICPR_OFFSET)) = (1u << ((uint32_t) (int_num & 0x1F)));
 }
 
-/*! \brief Force an interrupt to be pending on the executing core
+/*! \brief 実行中のコアにおける割り込みを強制的に保留する
  *  \ingroup hardware_irq
  *
- * This should generally not be used for IRQs connected to hardware.
+ * この関数は一般にハードウェアに接続されているIRQには使用する
+ * べきではありません。
  *
- * \param num Interrupt number \ref interrupt_nums
+ * \param num 割り込み場号 \ref interrupt_nums
  */
 void irq_set_pending(uint num);
 
 
-/*! \brief Perform IRQ priority initialization for the current core
+/*! \brief 現在のコアにおけるIRQ優先度の初期化を行う
  *
- * \note This is an internal method and user should generally not call it.
+ * \note これは内部関数であり、一般にユーザはこの関数を呼び出すべきでは
+ * ありません。
  */
 void irq_init_priorities(void);
 
-/*! \brief Claim ownership of a user IRQ on the calling core
+/*! \brief 呼び出したコアにおけるユーザIRQの所有権を主張する
  *  \ingroup hardware_irq
- *  
- * User IRQs are numbered 26-31 and are not connected to any hardware, but can be triggered by \ref irq_set_pending.
  *
- * \note User IRQs are a core local feature; they cannot be used to communicate between cores. Therfore all functions
- * dealing with Uer IRQs affect only the calling core
- * 
- * This method explicitly claims ownership of a user IRQ, so other code can know it is being used.
+ * ユーザIRQの番号は26-31であり、ハードウェアには接続されていませんが、
+ * \ref irq_set_pending によりトリガーすることができます。
  *
- * \param irq_num the user IRQ to claim
+ * \note ユーザIRQはコアローカルの機能です。コア間の通信には使用できません。
+ * したがって、ユーザIRQを扱うすべての関数は呼び出したコアにしか影響しません。
+ *
+ * この関数はユーザIRQの所有権を明示的に主張するので、他のコードはそれが
+ * 使用されていることを知ることができます。
+ *
+ * \param irq_num 主張するユーザIRQ
  */
 void user_irq_claim(uint irq_num);
 
-/*! \brief Mark a user IRQ as no longer used on the calling core
- *  \ingroup hardware_irq
+/*! \brief ユーザIrqを呼び出しコアでもはや使用していないものとしてマークするMark a user IRQ as no longer used on the calling core *  \ingroup hardware_irq
  *
- * User IRQs are numbered 26-31 and are not connected to any hardware, but can be triggered by \ref irq_set_pending.
+ * ユーザIRQの番号は26-31であり、ハードウェアには接続されていませんが、
+ * \ref irq_set_pending によりトリガーすることができます。
  *
- * \note User IRQs are a core local feature; they cannot be used to communicate between cores. Therfore all functions
- * dealing with Uer IRQs affect only the calling core
- * 
- * This method explicitly releases ownership of a user IRQ, so other code can know it is free to use.
- * 
- * \note it is customary to have disabled the irq and removed the handler prior to calling this method.
+ * \note ユーザIRQはコアローカルの機能です。コア間の通信には使用できません。
+ * したがって、ユーザIRQを扱うすべての関数は呼び出したコアにしか影響しません。
  *
- * \param irq_num the irq irq_num to unclaim
+ * この関数はユーザIRQの所有権を明示的に解放するので、他のコードはその番号が
+ * 開いていることを知ることができます。
+ *
+ * \note 通常、この関数を呼び出す前に、irqを無効にし、ハンドラを削除して
+ * おきます。
+ *
+ * \param irq_num 解除するirq番号
  */
 void user_irq_unclaim(uint irq_num);
 
-/*! \brief Claim ownership of a free user IRQ on the calling core
+/*! \brief 呼び出したコアの空いているユーザIRQの所有権を主張する
  *  \ingroup hardware_irq
- *  
- * User IRQs are numbered 26-31 and are not connected to any hardware, but can be triggered by \ref irq_set_pending.
  *
- * \note User IRQs are a core local feature; they cannot be used to communicate between cores. Therfore all functions
- * dealing with Uer IRQs affect only the calling core
- * 
- * This method explicitly claims ownership of an unused user IRQ if there is one, so other code can know it is being used.
+ * ユーザIRQの番号は26-31であり、ハードウェアには接続されていませんが、
+ * \ref irq_set_pending によりトリガーすることができます。
  *
- * \param required if true the function will panic if none are available
- * \return the user IRQ number or -1 if required was false, and none were free
+ * \note ユーザIRQはコアローカルの機能です。コア間の通信には使用できません。
+ * したがって、ユーザIRQを扱うすべての関数は呼び出したコアにしか影響しません。
+ *
+ * この関数は未使用のユーザIRQの所有権を明示的に主張するので、他のコードは
+ * それが使用されていることを知ることができます。
+ *
+ * \param required 利用可能なirqがない場合に関数をパニックさせる場合は true
+ * \return ユーザIRQ番号, または、要求が失敗し、空きがない場合は -1
  */
 int user_irq_claim_unused(bool required);
 
-/*
-*! \brief Check if a user IRQ is in use on the calling core
+/*! \brief ユーザIRQが呼び出したコアで使用されているかチェックする
  *  \ingroup hardware_irq
- *  
- * User IRQs are numbered 26-31 and are not connected to any hardware, but can be triggered by \ref irq_set_pending.
  *
- * \note User IRQs are a core local feature; they cannot be used to communicate between cores. Therfore all functions
- * dealing with Uer IRQs affect only the calling core
+ * ユーザIRQの番号は26-31であり、ハードウェアには接続されていませんが、
+ * \ref irq_set_pending によりトリガーすることができます。
  *
- * \param irq_num the irq irq_num
- * \return true if the irq_num is claimed, false otherwise
+ * \note ユーザIRQはコアローカルの機能です。コア間の通信には使用できません。
+ * したがって、ユーザIRQを扱うすべての関数は呼び出したコアにしか影響しません。
+ *
+ * \param irq_num irq番号
+ * \return irq_numが使用中の場合は true, そうでなければ false
  * \sa user_irq_claim
  * \sa user_irq_unclaim
  * \sa user_irq_claim_unused
