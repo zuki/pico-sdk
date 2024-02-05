f95e0e04b558bf231db81f19c18f23716c9749c8
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-02-05 17:33:02 +0900

    pico_float apiを翻訳

--- src/rp2_common/pico_float/include/pico/float.h
+++ src/rp2_common/pico_float/include/pico/float.h
@@ -19,16 +19,15 @@ extern "C" {
 /** \file float.h
 * \defgroup pico_float pico_float
 *
-* Optimized single-precision floating point functions
+* \brief 最適化された単精度浮動小数点関数
 *
-* (Replacement) optimized implementations are provided of the following compiler built-ins
-* and math library functions:
+* (代替）次のコンパイラ組み込み関数と数学ライブラリ関数の最適化実装が提供されています。
 *
 * - __aeabi_fadd, __aeabi_fdiv, __aeabi_fmul, __aeabi_frsub, __aeabi_fsub, __aeabi_cfcmpeq, __aeabi_cfrcmple, __aeabi_cfcmple, __aeabi_fcmpeq, __aeabi_fcmplt, __aeabi_fcmple, __aeabi_fcmpge, __aeabi_fcmpgt, __aeabi_fcmpun, __aeabi_i2f, __aeabi_l2f, __aeabi_ui2f, __aeabi_ul2f, __aeabi_f2iz, __aeabi_f2lz, __aeabi_f2uiz, __aeabi_f2ulz, __aeabi_f2d, sqrtf, cosf, sinf, tanf, atan2f, expf, logf
 * - ldexpf, copysignf, truncf, floorf, ceilf, roundf, asinf, acosf, atanf, sinhf, coshf, tanhf, asinhf, acoshf, atanhf, exp2f, log2f, exp10f, log10f, powf, hypotf, cbrtf, fmodf, dremf, remainderf, remquof, expm1f, log1pf, fmaf
 * - powintf, sincosf (GNU extensions)
 *
-* The following additional optimized functions are also provided:
+* さらに次の最適化関数も提供されています。
 *
 * - fix2float, ufix2float, fix642float, ufix642float, float2fix, float2ufix, float2fix64, float2ufix64, float2int, float2int64, float2int_z, float2int64_z
 */
