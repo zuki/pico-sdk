9b7c344a029749b309b2f76347d15f57da910b2d
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-02-05 17:29:53 +0900

    hardware_pll apiを翻訳

--- src/rp2_common/hardware_pll/include/hardware/pll.h
+++ src/rp2_common/hardware_pll/include/hardware/pll.h
@@ -17,13 +17,13 @@ extern "C" {
 /** \file hardware/pll.h
  *  \defgroup hardware_pll hardware_pll
  *
- * Phase Locked Loop control APIs
+ * \brief PLL (Phase Locked Loop) 制御API
  *
- * There are two PLLs in RP2040. They are:
- *   - pll_sys - Used to generate up to a 133MHz system clock
- *   - pll_usb - Used to generate a 48MHz USB reference clock
+ * RP2040には次の2つのPLLがあります。
+ *   - pll_sys - 最大 133MHz のシステムクロックの生成に使用します
+ *   - pll_usb - 48MHzのUSB基準クロックの生成に使用します
  *
- * For details on how the PLLs are calculated, please refer to the RP2040 datasheet.
+ * PLLの計算方法についての詳細はRP2040のデータシートを参照してください。
  */
 
 typedef pll_hw_t *PLL;
@@ -47,23 +47,23 @@ typedef pll_hw_t *PLL;
 #endif
 #endif
 
-/*! \brief Initialise specified PLL.
+/*! \brief 指定したPLLを初期化する.
  *  \ingroup hardware_pll
- * \param pll pll_sys or pll_usb
- * \param ref_div Input clock divider.
- * \param vco_freq  Requested output from the VCO (voltage controlled oscillator)
- * \param post_div1 Post Divider 1 - range 1-7. Must be >= post_div2
- * \param post_div2 Post Divider 2 - range 1-7
+ * \param pll pll_sys か pll_usb
+ * \param ref_div 入力クロック除算器.
+ * \param vco_freq  希望するVCO (voltage controlled oscillator) 出力
+ * \param post_div1 分周比 1 - 範囲は 1-7. post_div1 >= post_div2 でなければならない
+ * \param post_div2 分周比 2 - 範囲は 1-7
  */
 void pll_init(PLL pll, uint ref_div, uint vco_freq, uint post_div1, uint post_div2);
 
-/*! \brief Release/uninitialise specified PLL.
+/*! \brief 指定したPLLを解放/未初期化する.
  *  \ingroup hardware_pll
  *
- * This will turn off the power to the specified PLL. Note this function does not currently check if
- * the PLL is in use before powering it off so should be used with care.
+ * 指定したPLLの電源をオフにする。この関数は電源を切る前に現在PLLが使用中で
+ * あるかチェックしないので、使用には注意が必要です。
  *
- * \param pll pll_sys or pll_usb
+ * \param pll pll_sys か pll_usb
  */
 void pll_deinit(PLL pll);
 
