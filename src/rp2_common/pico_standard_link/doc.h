ecf21aadd502effc74a7d3f6ed3653592f311eb5
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-03-09 17:04:44 +0900

    pico_standard_linkを翻訳

--- src/rp2_common/pico_standard_link/doc.h
+++ src/rp2_common/pico_standard_link/doc.h
@@ -1,10 +1,10 @@
 /**
  * \defgroup pico_standard_link pico_standard_link
- * \brief Standard link step providing the basics for creating a runnable binary
- * 
- * This includes
- *   - C runtime initialization
- *   - Linker scripts for 'default', 'no_flash', 'blocked_ram' and 'copy_to_ram' binaries
- *   - 'Binary Information' support
- *   - Linker option control
+ * \brief 実行可能バイナリを作成するための基本を提供する標準的なリンクステップ.
+ *
+ * これには以下が含まれます。
+ *   - Cランタイムの初期化
+ *   - 'default', 'no_flash', 'blocked_ram', 'copy_to_ram'の各バイナリをサック制するためのリンカスクリプト
+ *   - 'バイナリ情報'のサポート
+ *   - リンカのオプション管理
  */
